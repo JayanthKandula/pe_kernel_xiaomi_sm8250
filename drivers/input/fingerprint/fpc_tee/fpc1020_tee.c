8bd3a92ac59d5e5034d08f74b12da0234dafa2f0
UtsavBalar1231 <utsavbalar1231@gmail.com>
2021-04-27 11:20:24 +0200

    drivers: Remove xiaomi early fingerprint wakeup optimization
    Change-Id: Ic732613398c48990a34de239682f3386ccc7e8c2
    Co-Authored-by: Dyneteve <dyneteve@hentaios.com>
    Signed-off-by: UtsavBalar1231 <utsavbalar1231@gmail.com>

--- drivers/input/fingerprint/fpc_tee/fpc1020_tee.c
+++ drivers/input/fingerprint/fpc_tee/fpc1020_tee.c
@@ -25,8 +25,6 @@
  * as published by the Free Software Foundation.
  */
 
-#define FPC_DRM_INTERFACE_WA
-
 #define CONFIG_FINGERPRINT_FP_VREG_CONTROL
 #define pr_fmt(fmt) KBUILD_MODNAME ": " fmt
 
@@ -666,6 +664,7 @@ static ssize_t wakeup_enable_set(struct device *dev,
 	ssize_t ret = count;
 
 	mutex_lock(&fpc1020->lock);
+
 	mutex_unlock(&fpc1020->lock);
 
 	return ret;
@@ -802,14 +801,12 @@ static struct attribute *attributes[] = {
 	&dev_attr_power_cfg.attr,
 	&dev_attr_irq.attr,
 	&dev_attr_vendor.attr,
-	&dev_attr_fingerdown_wait.attr,
 	NULL
 };
 
 static const struct attribute_group attribute_group = {
 	.attrs = attributes,
 };
-
 
 static irqreturn_t fpc1020_irq_handler(int irq, void *handle)
 {
@@ -818,10 +815,12 @@ static irqreturn_t fpc1020_irq_handler(int irq, void *handle)
 	dev_dbg(fpc1020->dev, "%s\n", __func__);
 
 	mutex_lock(&fpc1020->lock);
+
 	if (atomic_read(&fpc1020->wakeup_enabled)) {
 		fpc1020->nbr_irqs_received++;
 		__pm_wakeup_event(fpc1020->ttw_wl, (FPC_TTW_HOLD_TIME));
 	}
+
 	mutex_unlock(&fpc1020->lock);
 
 	sysfs_notify(&fpc1020->dev->kobj, NULL, dev_attr_irq.attr.name);
@@ -862,7 +861,6 @@ static int fpc1020_probe(struct platform_device *pdev)
 	struct device *dev = &pdev->dev;
 	int rc = 0;
 	size_t i;
-
 	struct device_node *np = dev->of_node;
 	struct fpc1020_data *fpc1020 = devm_kzalloc(dev, sizeof(*fpc1020),
 						    GFP_KERNEL);
@@ -917,14 +915,8 @@ static int fpc1020_probe(struct platform_device *pdev)
 	fpc1020->irq_requested = false;
 	fpc1020->gpios_requested = false;
 	device_init_wakeup(dev, 1);
-/*
-	if (of_property_read_bool(dev->of_node, "fpc,enable-wakeup")) {
-		irqf |= IRQF_NO_SUSPEND;
-		device_init_wakeup(dev, 1);
-	}
-*/
-	mutex_init(&fpc1020->lock);
 
+	mutex_init(&fpc1020->lock);
 	fpc1020->ttw_wl = wakeup_source_register(dev, "fpc_ttw_wl");
 	if (!fpc1020->ttw_wl)
 		return -ENOMEM;
@@ -965,7 +957,6 @@ exit:
 static int fpc1020_remove(struct platform_device *pdev)
 {
 	struct fpc1020_data *fpc1020 = platform_get_drvdata(pdev);
-
 	sysfs_remove_group(&pdev->dev.kobj, &attribute_group);
 	mutex_destroy(&fpc1020->lock);
 	wakeup_source_unregister(fpc1020->ttw_wl);
